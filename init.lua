bc12d6f7d1bf474357f29469f4cd47eec9da2844
Tiago Baptista Noronha <tiago.bn@gmail.com>
2026-02-24 16:49:46 -0300

    feat: gitlab support

--- init.lua
+++ init.lua
@@ -942,6 +942,42 @@ require('lazy').setup({
   },
 
   {
+    "ellisonleao/dotenv.nvim",
+    config = function()
+      require("dotenv").setup()
+      local global_env = vim.fn.stdpath("config") .. "/.env"
+      if vim.fn.filereadable(global_env) == 1 then
+        vim.cmd("Dotenv " .. global_env)
+      end
+    end
+  },
+
+  {
+    "harrisoncramer/gitlab.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "stevearc/dressing.nvim", 
+      "nvim-tree/nvim-web-devicons"
+    },
+    enabled = true,
+    build = function () require("gitlab.server").build(true) end, 
+    config = function()
+      require("gitlab").setup()
+    end,
+    keys = {
+      { "<leader>gls", "<cmd>lua require('gitlab').summary()<CR>", desc = "GitLab Summary" },
+      { "<leader>glr", "<cmd>lua require('gitlab').review()<CR>", desc = "GitLab Review" },
+      { "<leader>glA", "<cmd>lua require('gitlab').approve()<CR>", desc = "GitLab Approve" },
+      { "<leader>glR", "<cmd>lua require('gitlab').revoke()<CR>", desc = "GitLab Revoke Approval" },
+      { "<leader>glc", "<cmd>lua require('gitlab').create_comment()<CR>", desc = "GitLab Create Comment" },
+      { "<leader>glp", "<cmd>lua require('gitlab').pipeline()<CR>", desc = "GitLab Pipeline" },
+      { "<leader>gld", "<cmd>lua require('gitlab').toggle_discussions()<CR>", desc = "GitLab Discussions" },
+    }
+  },
+
+  {
     "nvim-neo-tree/neo-tree.nvim",
     version = "*",
     lazy = false,
