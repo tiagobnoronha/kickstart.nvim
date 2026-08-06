5b750e8fc6279e149c38e3b34ac5b8848da8a53f
Tiago Baptista Noronha <tbn87@jfrs.jus.br>
2026-08-06 16:40:22 -0300

    feat: add PHP stubs path and WordPress indentation ftplugin
    Configure intelephense to include local PHP stub definitions and add
    a php ftplugin enforcing tab-based indentation for WordPress code.

---
+++ ftplugin/php.lua
@@ -0,0 +1,5 @@
+-- Configuração ideal para o padrão WordPress (Tabs reais com tamanho visual 4)
+vim.opt.expandtab = false   -- NÃO converte tab em espaços (WordPress exige tabs)
+vim.opt.tabstop = 4         -- Largura visual que um caractere de TAB ocupa
+vim.opt.shiftwidth = 4      -- Número de espaços usados para cada etapa de indentação automática
+vim.opt.softtabstop = 4     -- Controla o comportamento do TAB/Backspace no modo de inserção
